509baad030c30bf046f9671b397c6eae31d80568
lambert-lincoln <l4mbertlim@gmail.com>
2025-10-09 11:55:35 +0700

    implementation of st.metrics

--- debugging.py
+++ debugging.py
@@ -9,7 +9,7 @@ ticker_to_test = "AAPL"
 stock = yf.Ticker(ticker=ticker_to_test)
 fetcher = DataFetcher(ticker_to_test)
 price_data = fetcher.get_technical()
-st.write(price_data)
 st.plotly_chart(figure.plot_chart(price_data), use_container_width=True, height=800)
 st.write(price_data)
+st.metric(label="Opening Price", value=price_data["Open"].iloc[-1])
 
