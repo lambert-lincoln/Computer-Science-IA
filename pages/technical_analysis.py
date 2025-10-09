bbfe621f41de1148325cd6083da46bd2a303534b
lambert-lincoln <l4mbertlim@gmail.com>
2025-10-09 11:48:30 +0700

    debugging

--- pages/technical_analysis.py
+++ pages/technical_analysis.py
@@ -6,6 +6,7 @@ def display_price_chart(ticker: str):
     fetcher = DataFetcher(ticker)
     figure = Figure()
     
+    col1, col2, col3 = st.columns(3)
     price_data = fetcher.get_technical()
     
     st.title(f"Price Data {ticker}")
@@ -13,6 +14,9 @@ def display_price_chart(ticker: str):
     st.plotly_chart(figure.plot_chart(price_data), use_container_width=True)
     st.divider()
     st.header("Price Data")
+    st.data_editor(price_data)
+    with col1:
+        st.metric(label="Opening Price", value=f"")
     st.markdown("If you need help interpreting this data, you can ask your AI Financial Mentor")
     st.button(label="Ask AI Financial Mentor", icon="↗")
       
