11dc8e3bfe7ff25a39d2e00e3215671be2abda41
lambert-lincoln <l4mbertlim@gmail.com>
2026-01-06 11:13:04 +0700

    Remove secrets and credentials

--- APIkey.py
+++
@@ -1,8 +0,0 @@
-class _ApiKey:
-    def __init__(self):
-        self.api_dict = {
-            "GLM 4.5 API KEY" : "a2c42996f098427fa00d7bb2e621f765.JtKbmR2FDvLRYQgc",
-        }
-    def get_ZAI_key(self):
-        return self.api_dict.get('GLM 4.5 API KEY', 'key not found')
-        
